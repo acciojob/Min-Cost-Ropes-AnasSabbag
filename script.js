e61ba0186576ea2c7a11f10853ed1763efafbde6
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-21 12:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort();
+	let a=0,b=0;
+	while(arr.length>1){
+		b=arr[0];
+		arr.shift();
+		b+=arr[0];
+		arr.shift();
+		a= a +b;
+		arr.push(b);
+		arr.sort();
+	}
+	return a;
 }
 
 module.exports=mincost;
